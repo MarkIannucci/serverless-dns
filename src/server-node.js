2869656a213fae80ca179008cbfe90f9e7ef0114
Mark Iannucci <mark.iannucci@gmail.com>
2023-03-29 05:10:39 +0000

    comment out the machinesHeartbeat hack

--- src/server-node.js
+++ src/server-node.js
@@ -614,10 +614,11 @@ function serveTCP(socket) {
   const [flag, host] = ["", "ignored.example.com"];
   const sb = new ScratchBuffer();
 
+  machinesHeartbeat();
   log.d("----> DoT Cleartext request", host, flag);
 
   socket.on("data", (data) => {
-    machinesHeartbeat();
+    //machinesHeartbeat();
     handleTCPData(socket, data, sb, host, flag);
   });
 }
